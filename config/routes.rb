91d3cbccfccd50d8407d8420d3a8cd662a48fa42
bentonwong <bentonwong@gmail.com>
2017-08-28 17:01:12 -0700

    add javascript, routes, list and destroy methods for controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :uploads
+  resources :uploads, only: [:index, :create, :destroy] do
+   collection do
+     get :list #list_uploads_url
+   end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
